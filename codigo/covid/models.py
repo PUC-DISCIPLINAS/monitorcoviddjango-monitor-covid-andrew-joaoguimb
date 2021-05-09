c6715b8ec943d0d5801658cd6d8518aa11428fb3
JoaoGuiMB <juaogui159@gmail.com>
2021-05-09 12:03:38 -0300

    feat: start models

--- codigo/covid/models.py
+++ codigo/covid/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
-# Create your models here.
-class Covid(models.Model):
 
+# Create your models here.
+
+
+class Country(models.Model):
+    name = models.TextField()
+
+
+class Covid(models.Model):
+    countryId = models.ForeignKey(Country, on_delete=models.CASCADE)
+    cases = models.IntegerField()
+    deaths = models.IntegerField()
+    recovered = models.IntegerField()
